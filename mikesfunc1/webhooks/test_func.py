8e86545464239d40bc33fe1ec9b2d46d23e1c94f
mikespub <github@mikespub.net>
2019-10-29 01:37:02 +0100

    add test_func for webhooks

---
+++ mikesfunc1/webhooks/test_func.py
@@ -0,0 +1,23 @@
+import unittest
+import azure.functions as func
+from . import main
+
+
+class TestFunction(unittest.TestCase):
+    def test_name(self):
+        # Construct a mock HTTP request.
+        req = func.HttpRequest(
+            method='GET',
+            body=None,
+            url='/webhooks', 
+            params={'name': 'Test'})
+
+        # Call the function.
+        resp = main(req)
+        #print(resp.get_body())
+
+        # Check the output.
+        self.assertEqual(
+            resp.get_body(),
+            b'Hello Test!'
+        )
