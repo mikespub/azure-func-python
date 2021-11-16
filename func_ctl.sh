2aa8f0864547301bb18b4cab2e542cd92df4b34c
mikespub <github@mikespub.net>
2021-11-16 19:11:33 +0000

    update docs

--- func_ctl.sh
+++ func_ctl.sh
@@ -16,6 +16,20 @@ case "$1" in
 	;;
 	edit)
 	;;
+	docs)
+		cd ..
+		wget https://github.com/Azure/azure-functions-python-worker/raw/dev/README.md -O README.in
+		echo "Source: [Azure/azure-functions-python-worker](https://github.com/Azure/azure-functions-python-worker)" > README.md
+		echo >> README.md
+		cat README.in >> README.md
+		rm README.in
+		wget https://raw.githubusercontent.com/MicrosoftDocs/azure-docs/master/articles/azure-functions/create-first-function-cli-python.md -O create-first-function-cli-python.in
+		echo "Source: [MicrosoftDocs/azure-docs](https://github.com/MicrosoftDocs/azure-docs/tree/master/articles/azure-functions)" > create-first-function-cli-python.md
+		echo >> create-first-function-cli-python.md
+		cat create-first-function-cli-python.in >> create-first-function-cli-python.md
+		rm create-first-function-cli-python.in
+
+	;;
 	test)
 		pytest
 		deactivate
@@ -25,7 +39,7 @@ case "$1" in
 		deactivate
 	;;
 	*)
-		echo "Usage: $0 {start|edit|publish|new|test}"
+		echo "Usage: $0 {start|edit|publish|new|test|docs}"
 		exit 1
 	;;
 esac
